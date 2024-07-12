30273da3d3b530a1653ac42b35d4d43ddbd8f05a
Brunoso <84466539+Brunoso0@users.noreply.github.com>
2024-07-12 16:34:30 -0300

    Commit para terminar em casa

--- js/fade.js
+++ js/fade.js
@@ -13,3 +13,27 @@ document.addEventListener('DOMContentLoaded', function() {
 	window.addEventListener('scroll', checkPosition);
 	checkPosition(); // Verifica a posição inicial
 });
+
+// scripts.js
+let angle = 0;
+const carouselItems = document.querySelectorAll('.carousel-item');
+let currentIndex = 0;
+
+function updateCarousel() {
+    angle -= 90;
+    document.querySelector('.carousel').style.transform = `rotateY(${angle}deg)`;
+    
+    carouselItems.forEach((item, index) => {
+        item.classList.remove('active');
+        if (index === (currentIndex % 4 + 4) % 4) {
+            item.classList.add('active');
+        }
+    });
+
+    currentIndex++;
+}
+
+setInterval(updateCarousel, 6000); // Change slide every 3 seconds
+
+// Initialize the first item as active
+carouselItems[currentIndex].classList.add('active');
